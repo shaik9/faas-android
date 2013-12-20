2b2fc1bba002ee859aa86bae988437100bdd5ae9
Prasad Konduru <prasad@eggheadcreative.com>
2013-12-20 16:56:01 +0530

    Added method to show feedback in Faas.class

--- src/main/java/com/ehc/faas/Faas.java
+++ src/main/java/com/ehc/faas/Faas.java
@@ -1,12 +1,19 @@
 package com.ehc.faas;
 
-public class Faas {
+import android.app.Activity;
+import android.content.Context;
 
+public class Faas {
   static String apiKey;
   static String channelName;
 
   public static void register(String channelName, String apiKey) {
     Faas.apiKey = apiKey;
     Faas.channelName = channelName;
+  }
+
+  public static void showFeedbackView(Context context) {
+    FeedbackView feedbackView = new FeedbackView(context);
+    ((Activity) (context)).setContentView(feedbackView);
   }
 }
